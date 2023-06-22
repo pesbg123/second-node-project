12c4bb3e791bb3e211f2584f8a63e62a5fb501a6
marshmello <pesbg123@gmail.com>
2023-06-23 06:18:50 +0900

    real done

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../schemas/users.js');
 const mongoose = require('mongoose');
+const { ObjectId } = require('mongodb');
 
 const authMiddleware = require('../middlewares/auth-middleware');
 
@@ -17,29 +18,47 @@ router.get('/users/me', authMiddleware, async (req, res) => {
 // 회원가입 API
 router.post('/users', async (req, res) => {
   const { email, nickname, password, confirmPassword } = req.body;
-  // 패스워드 확인
-  if (password !== confirmPassword) {
-    res.status(400).json({
-      errorMessage: '패스워드와 전달된 패스워드 확인값이 일치하지 않습니다.',
+
+  // 닉네임 유효성 검사
+  if (!nickname.match(/^[a-zA-Z0-9]{3,50}$/)) {
+    return res.status(400).json({
+      error: '닉네임은 영어나 숫자로만 이루어지게 작성해 주세요.',
     });
-    return;
   }
 
-  const isExistUser = await User.findOne({
-    $or: [{ email }, { nickname }],
-  });
+  // 패스워드 유효성 검사
+  if (password.length < 4) {
+    return res.status(400).json({
+      errorMessage: '패스워드를 4글자 이상 작성해 주세요.',
+    });
+  }
+
+  if (password.includes(nickname)) {
+    return res.status(400).json({
+      errorMessage: '닉네임이 패스워드에 포함될 수 없습니다.',
+    });
+  }
+
+  if (password !== confirmPassword) {
+    return res.status(400).json({
+      errorMessage: '패스워드와 전달된 패스워드 확인값이 일치하지 않습니다.',
+    });
+  }
+
+  const isExistUser = await User.findOne({ $or: [{ email }, { nickname }] });
   if (isExistUser) {
-    res.status(400).json({
+    return res.status(400).json({
       errorMessage: '이메일 또는 닉네임이 이미 사용중입니다.',
     });
-    return;
   }
 
   const userId = new ObjectId().toHexString();
   const user = new User({ userId, email, nickname, password });
   await user.save(); // DB에 저장
 
-  return res.status(201).json({});
+  return res.status(201).json({
+    message: '회원가입이 정상적으로 완료되었습니다.',
+  });
 });
 
 module.exports = router;
